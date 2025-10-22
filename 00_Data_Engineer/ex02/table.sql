b74d7a61d015b4331609227c70732eee8a1a29b0
lmunoz-q <lmunoz-q@student.42.fr>
2025-10-22 19:49:04 +0200

    ok

--- 00_Data_Engineer/ex02/table.sql
+++ 00_Data_Engineer/ex02/table.sql
@@ -2,7 +2,7 @@ CREATE TABLE data_2022_oct (
 	event_time		TIMESTAMPTZ,
 	event_type		VARCHAR(40),
 	product_id		INTEGER,
-	price			NUMERIC(100000000000, 2),
+	price			NUMERIC(1000, 2),
 	user_id			BIGINT,
 	user_session	UUID
 );
@@ -11,7 +11,7 @@ CREATE TABLE data_2022_nov (
 	event_time		TIMESTAMPTZ,
 	event_type		VARCHAR(40),
 	product_id		INTEGER,
-	price			NUMERIC(100000000000, 2),
+	price			NUMERIC(1000, 2),
 	user_id			BIGINT,
 	user_session	UUID
 );
@@ -20,7 +20,7 @@ CREATE TABLE data_2022_dec (
 	event_time		TIMESTAMPTZ,
 	event_type		VARCHAR(40),
 	product_id		INTEGER,
-	price			NUMERIC(100000000000, 2),
+	price			NUMERIC(1000, 2),
 	user_id			BIGINT,
 	user_session	UUID
 );
@@ -29,7 +29,24 @@ CREATE TABLE data_2023_jan (
 	event_time		TIMESTAMPTZ,
 	event_type		VARCHAR(40),
 	product_id		INTEGER,
-	price			NUMERIC(100000000000, 2),
+	price			NUMERIC(1000, 2),
 	user_id			BIGINT,
 	user_session	UUID
 );
+
+
+COPY data_2022_oct(event_time,event_type,product_id,price,user_id,user_session)
+FROM '<chemin_interne_dans_le_conteneur>'
+WITH (FORMAT csv, HEADER true);
+
+COPY data_2022_nov(event_time,event_type,product_id,price,user_id,user_session)
+FROM '<chemin_interne_dans_le_conteneur>'
+WITH (FORMAT csv, HEADER true);
+
+COPY data_2022_dec(event_time,event_type,product_id,price,user_id,user_session)
+FROM '<chemin_interne_dans_le_conteneur>'
+WITH (FORMAT csv, HEADER true);
+
+COPY data_2023_jan(event_time,event_type,product_id,price,user_id,user_session)
+FROM '<chemin_interne_dans_le_conteneur>'
+WITH (FORMAT csv, HEADER true);

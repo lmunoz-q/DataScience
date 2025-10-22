be96118c83b6c595a12c02a0f3893f82362cbfad
lmunoz-q <lmunoz-q@student.42.fr>
2025-10-22 18:53:43 +0200

    create tables

---
+++ 00_Data_Engineer/ex02/table.sql
@@ -0,0 +1,35 @@
+CREATE TABLE data_2022_oct (
+	event_time		TIMESTAMPTZ,
+	event_type		VARCHAR(40),
+	product_id		INTEGER,
+	price			NUMERIC(100000000000, 2),
+	user_id			BIGINT,
+	user_session	UUID
+);
+
+CREATE TABLE data_2022_nov (
+	event_time		TIMESTAMPTZ,
+	event_type		VARCHAR(40),
+	product_id		INTEGER,
+	price			NUMERIC(100000000000, 2),
+	user_id			BIGINT,
+	user_session	UUID
+);
+
+CREATE TABLE data_2022_dec (
+	event_time		TIMESTAMPTZ,
+	event_type		VARCHAR(40),
+	product_id		INTEGER,
+	price			NUMERIC(100000000000, 2),
+	user_id			BIGINT,
+	user_session	UUID
+);
+
+CREATE TABLE data_2023_jan (
+	event_time		TIMESTAMPTZ,
+	event_type		VARCHAR(40),
+	product_id		INTEGER,
+	price			NUMERIC(100000000000, 2),
+	user_id			BIGINT,
+	user_session	UUID
+);

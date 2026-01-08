8da73f6412f9c5adb1f3dc81589e814936f30f8d
aorus <lmunoz-q@student.42.fr>
2026-01-08 12:05:47 +0100

    wow

--- 00_Data_Engineer/ex03/automatic_table.py
+++ 00_Data_Engineer/ex03/automatic_table.py
@@ -1,21 +1,25 @@
 import glob, os, subprocess, shlex
 
-CSV_DIR = "customer"
-DB_HOST = "db"
+CSV_DIR = "../../subject/customer/"
+DB_HOST = "localhost"
 DB_NAME = "piscineds"
 DB_USER = "lmunoz-q"
+PG_PASS = "mysecretpassword"
 
 DDL = """CREATE TABLE IF NOT EXISTS {table} (
-  event_time TIMESTAMPTZ NOT NULL,
-  event_type VARCHAR(40) NOT NULL,
-  product_id INTEGER NOT NULL,
-  price NUMERIC(1000,2) NOT NULL,
-  user_id BIGINT NOT NULL,
+  event_time   TIMESTAMPTZ NOT NULL,
+  event_type   VARCHAR(40) NOT NULL,
+  product_id   INTEGER NOT NULL,
+  price        NUMERIC(1000,2) NOT NULL,
+  user_id      BIGINT NOT NULL,
   user_session UUID NOT NULL
 );"""
 
-for path in glob.glob(os.path.join(CSV_DIR, "*.csv")):
-    table = os.path.splitext(os.path.basename(path))[0]
+env_with_pass = {**os.environ, "PGPASSWORD": PG_PASS}
 
-    cmd1 = f'psql -h {DB_HOST} -U {DB_USER} -d {DB_NAME} -c "{DDL.format(table=table)}"'
-    subprocess.run(shlex.split(cmd1), check=True)
+csvs = glob.glob(os.path.join(CSV_DIR, "*.csv"))
+
+for path in csvs:
+  table = os.path.splitext(os.path.basename(path))[0]
+  cmd1 = f'psql -h {DB_HOST} -U {DB_USER} -d {DB_NAME} -c "{DDL.format(table=table)}"'
+  subprocess.run(shlex.split(cmd1), check=True, env=env_with_pass)

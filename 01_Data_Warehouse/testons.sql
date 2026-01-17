f62e8755867334bdf1f3c49dfb8dbbad1e06629b
aorus <lmunoz-q@student.42.fr>
2026-01-17 16:10:45 +0100

    test

---
+++ 01_Data_Warehouse/testons.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS test1 (
+  name			VARCHAR(40),
+  last_name		VARCHAR(40),
+  age			INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS test2 (
+  name			VARCHAR(40),
+  last_name		VARCHAR(40),
+  age			INTEGER
+);

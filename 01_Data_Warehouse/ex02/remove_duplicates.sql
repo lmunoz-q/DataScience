4ccc14bfea8eefaec57745ee5d8c10a2bda802f7
aorus <lmunoz-q@student.42.fr>
2026-02-24 15:45:28 +0100

    good remove

--- 01_Data_Warehouse/ex02/remove_duplicates.sql
+++ 01_Data_Warehouse/ex02/remove_duplicates.sql
@@ -1,11 +1,11 @@
 DELETE FROM customers a
 USING customers b
 WHERE
-  a.user_session = b.user_session
-  AND a.user_id = b.user_id
-  AND a.event_type = b.event_type
-  AND a.product_id = b.product_id
-  AND a.price = b.price
+  a.user_session	IS NOT DISTINCT FROM b.user_session
+  AND a.user_id		IS NOT DISTINCT FROM b.user_id
+  AND a.event_type	IS NOT DISTINCT FROM b.event_type
+  AND a.product_id	IS NOT DISTINCT FROM b.product_id
+  AND a.price		IS NOT DISTINCT FROM b.price
   AND a.event_time >= b.event_time
-  AND a.event_time <= b.event_time + INTERVAL '1 second'
+  AND a.event_time < b.event_time + INTERVAL '1 second'
   AND (a.event_time > b.event_time OR a.ctid > b.ctid);

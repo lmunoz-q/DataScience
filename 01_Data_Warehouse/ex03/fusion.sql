6e95fd50ef809999bdd5d950a04791d7b402ba5c
aorus <lmunoz-q@student.42.fr>
2026-05-04 15:47:00 +0200

    fini

--- 01_Data_Warehouse/ex03/fusion.sql
+++ 01_Data_Warehouse/ex03/fusion.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS items (
-  product_id   BIGINT PRIMARY KEY,
-  category_id  BIGINT,
+  product_id BIGINT,
+  category_id BIGINT,
   category_code TEXT,
-  brand        TEXT
+  brand TEXT
 );
 
--- faire la copie de {items}
+-- faire la copie sur pgadmin
 
 ALTER TABLE customers
   ADD COLUMN IF NOT EXISTS category_id BIGINT,
@@ -13,8 +13,14 @@ ALTER TABLE customers
   ADD COLUMN IF NOT EXISTS brand TEXT;
 
 UPDATE customers c
-SET category_id   = i.category_id,
-    category_code = i.category_code,
-    brand         = i.brand
-FROM items i
+SET
+  category_id = i.category_id,
+  category_code = i.category_code,
+  brand = i.brand
+FROM (
+  SELECT DISTINCT ON (product_id)
+    product_id, category_id, category_code, brand
+  FROM items
+  ORDER BY product_id
+) i
 WHERE c.product_id = i.product_id;
